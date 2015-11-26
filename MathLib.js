d088af9e05ea92bec574960f03f7f26f700029a8
Steven Zeiler <stevenzeiler@Stevens-MacBook-Pro.local>
2015-11-26 15:37:38 -0800

    [FEATURE] divide by zero

--- MathLib.js
+++ MathLib.js
@@ -1,4 +1,10 @@
 
+module.exports.divide = (num, den) => {
+	if (den === 0) {
+		throw new Error('denominator cannot be zero')
+	}
+}
+
 module.exports.add = (num1, num2) => {
 	return num1 + num2
 }
